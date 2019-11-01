698bea801d330f78fd973a2e255f3f128c7dc207
mettled <smettled@gmail.coom>
2019-11-01 21:49:19 +0300

    delete some file

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,5 @@
-import {
-  askName, askAnswer,
-} from './textMessage';
 import { rundomFunc, gcdFunc } from './mathFunction';
 
 export {
-  askName, askAnswer, rundomFunc, gcdFunc,
+  rundomFunc, gcdFunc,
 };
